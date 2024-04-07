7d96e03e5f7e0d5c7e7e473b853f39119ff662ee
Bohdan Poberezhnyi <poberboh@fit.cvut.cz>
2024-04-07 23:09:07 +0200

    dependencies added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,16 +33,30 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
+    implementation(libs.material)
+
+    implementation(libs.osmdroid.android)
+
+    implementation(libs.squareup.okhttp3)
+    implementation(libs.squareup.retrofit)
+    implementation(libs.squareup.retrofit.converter.gson)
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     implementation(libs.androidx.activity)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.preference)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
